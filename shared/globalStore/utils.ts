a929fa9216f6ab6c0e50a078dc9c31ae765fc788
aodaGit <aoda2826@qq.com>
2024-01-21 20:03:57 +0800

    update my profile

--- shared/globalStore/utils.ts
+++
@@ -1,7 +0,0 @@
-import { devtools, persist } from "zustand/middleware";
-import { immer } from "zustand/middleware/immer";
-// It can persist save data in localStorage
-const persistState = f => {
-  return devtools(persist(f, { name: "aaa" }));
-};
-export default persistState;
