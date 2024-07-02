07d617558290a631fe96bff27c2df7feae83d65e
aoda <aodazhang666@gmail.com>
2024-07-02 08:13:51 +0800

    feat : Update tutorials

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { ThemeProvider } from "next-themes";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Footer from "@/shared/components/Footer";
@@ -7,7 +6,6 @@ import Header from "@/shared/components/Header";
 import "@/shared/styles/global.scss";
 import globalStore from "@/store/globalStore";
 import ThemeKeys from "@/constants/themeKeys";
-// import styles from "@/shared/styles/global.scss";
 
 export default function RootLayout({
   children,
@@ -21,10 +19,8 @@ export default function RootLayout({
         <div className={`layout ${ThemeKeys?.[theme]}`}>
           <Header />
           <div className="content">
-            <ThemeProvider enableSystem={true}>
-              {children}
-              <SpeedInsights />
-            </ThemeProvider>
+            {children}
+            <SpeedInsights />
           </div>
           <Footer />
         </div>
