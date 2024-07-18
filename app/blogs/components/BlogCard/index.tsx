82bd4478ee39b73a6a3005121d0d991791ec15e5
aoda <aodazhang666@gmail.com>
2024-07-18 23:17:39 +0800

    feat : Update home page

--- app/blogs/components/BlogCard/index.tsx
+++ app/blogs/components/BlogCard/index.tsx
@@ -7,13 +7,16 @@ import styles from "./index.module.scss";
 const BlogCard: FC<PostItemType> = item => {
   return (
     <div className={styles.blogCard}>
-      <Image
+      {
+        item?.coverPath && <Image
         src={item?.coverPath}
         alt={""}
         className={styles.conver}
         width={260}
         height={180}
       />
+      }
+      
       <p className={styles.title}>{item.title}</p>
       <p className={styles.desc}>{item?.desc}</p>
     </div>
